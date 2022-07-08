f866df4709485c5ec079ae70febed17a093f7220
fed1k <allamurotovcoder@mail.ru>
2022-07-08 22:52:31 +0500

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, length: { minimum: 0 }, numericality: { only_integer: true }
+  validates :likes_counter, presence: true, length: { minimum: 0 }, numericality: { only_integer: true }
 
   def five_recent_comments
     Comment.all.order(created_at: :desc).limit(5)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts
+  validates :name, presence: true
+  validates :posts_counter, presence: true, length: { minimum: 0 }, numericality: { only_integer: true }
 
   def three_recent_posts
     Post.all.order(created_at: :desc).limit(3)
