fb71d8a04397e226bfeeb176915d2b6d1df5bbc5
fed1k <allamurotovcoder@mail.ru>
2022-07-15 03:05:19 +0500

    implement user_post design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,9 @@ class PostsController < ApplicationController
     @user = User.find(8)
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: 2, author_id: 8)
+  end
 end
