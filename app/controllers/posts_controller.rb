791753594f169c026be96b5138661f4d740a40dc
fed1k <allamurotovcoder@mail.ru>
2022-07-23 03:17:06 +0500

    add load_and_authorize_resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes([:comments])
@@ -16,7 +17,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    current_user = User.find_by_id(session[:current_user_id])
     @post = Post.new(post_params)
     @post.author_id = current_user.id
     @post.comments_counter = 0
